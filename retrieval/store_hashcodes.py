a360c8c954984c0a5b1bbabb9593e0cfd841e152
hcjin0816 <haochen_jin2022@163.com>
2025-01-24 16:33:18 +0800

    Update store_hashcodes.py

--- retrieval/store_hashcodes.py
+++ retrieval/store_hashcodes.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-root_dir = os.path.abspath(os.getcwd())  # xxx/MRAN
+root_dir = os.path.abspath(os.getcwd())  # xxx/DG_WSDH
 sys.path.append(root_dir)
 import argparse
 import csv
